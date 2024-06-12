7dca900ef63947d299ed689f1386d833913b656f
Poonam Gaigole <poonam.gaigole@gmail.com>
2024-06-12 16:11:20 +0530

    Use lucide react for customized icons and create Navigation

---
+++ utils/links.tsx
@@ -0,0 +1,13 @@
+import { AppWindow, AreaChart, Layers } from "lucide-react";
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+const links: Array<NavLink> = [
+  { href: "/add-job", label: "add job", icon: <Layers /> },
+  { href: "/jobs", label: "jobs", icon: <AppWindow /> },
+  { href: "/stats", label: "stats", icon: <AreaChart /> },
+];
